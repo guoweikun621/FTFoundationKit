352d0786cd91ef0f84da713bd54df01f3106b075
郭伟坤 <guoweikun621@gmail.com>
2016-10-12 14:49:41 +0800

    添加网络服务组

---
+++ Sources/NetworkServer/FTHttpManager.swift
@@ -0,0 +1,13 @@
+//
+//  FTHttpManager.swift
+//  FTFoundationKit
+//
+//  Created by 郭伟坤 on 2016/10/12.
+//  Copyright © 2016年 郭伟坤. All rights reserved.
+//
+
+import UIKit
+
+class FTHttpManager: NSObject {
+
+}
