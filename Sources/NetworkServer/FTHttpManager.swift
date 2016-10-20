64e7a953df83504a3898a6c4ac65783bf3c01193
郭伟坤 <guoweikun621@gmail.com>
2016-10-20 10:14:52 +0800

    更新Http Manager

--- Sources/NetworkServer/FTHttpManager.swift
+++ Sources/NetworkServer/FTHttpManager.swift
@@ -10,22 +10,12 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-
-@objc protocol FTHttpDelegate {
-    optional func requestStart(manager: FTHttpManager)
-    optional func requestDidSuccess(manager: FTHttpManager, result: [String: AnyObject]?)
-    optional func requestDidFailure(manager: FTHttpManager, error: NSError)
-}
-
 public class FTHttpManager: NSObject {
     
     var api: FTBasicApi
-    weak var delegate: FTHttpDelegate?
     
-    
-    init(api: FTBasicApi, delegate: FTHttpDelegate? = nil) {
+    init(api: FTBasicApi) {
         self.api = api
-        self.delegate = delegate
     }
     
     let defaultManager: Alamofire.Manager = {
@@ -54,15 +44,11 @@ public class FTHttpManager: NSObject {
     
     func request(method: Alamofire.Method) -> Alamofire.Request {
         let p = api.params
-        delegate?.requestStart?(self)
         let req = defaultManager.request(method, api.url.absoluteString!, parameters: p, encoding: .URLEncodedInURL, headers: nil)
-//        req.responseJSON { resp in
-//            self.disposeRequestResult(resp)
-//        }
         return req
     }
     
-    func upload(completion: (Manager.MultipartFormDataEncodingResult -> Void)?) {
+    func upload(complation: (result: Manager.MultipartFormDataEncodingResult) -> Void?) {
         if let uApi = api as? FTUploadApi {
             let files = uApi.filePaths
             
@@ -75,36 +61,14 @@ public class FTHttpManager: NSObject {
                 }, encodingCompletion: { res in
                     switch res {
                     case .Success(let request, let streamingFromDisk, let streamFileURL):
-                        debugPrint(request)
-                        debugPrint(streamingFromDisk)
-                        debugPrint(streamFileURL)
-                    case .Failure:
-                        debugPrint("")
+                        complation(result: .Success(request: request, streamingFromDisk: streamingFromDisk, streamFileURL: streamFileURL))
+                    case .Failure(let eType):
+                        complation(result: .Failure(eType))
                     }
             })
         }
         
     }
-    
-//    func disposeRequestResult(resp: Response<AnyObject, NSError>) -> Void {
-//        let result = resp.result
-//        if result.isSuccess {
-//            let json = JSON(result.value!)
-//            let code = json[HttpRespJsonKey.ResultCode].string ?? "0"
-//            let msg = json[HttpRespJsonKey.ResultMsg].string ?? "未知错误"
-//            if code == "000000" {
-//                delegate?.requestDidSuccess?(self, result: json[HttpRespJsonKey.Data].dictionaryObject)
-//            }
-//            else {
-//                let c = (code as NSString).integerValue
-//                delegate?.requestDidFailure?(self, error: NSError(domain: "com.forte.error", code: c, userInfo: [NSLocalizedFailureReasonErrorKey: msg]))
-//            }
-//        }
-//        else if result.isFailure {
-//            let error = result.error ?? NSError(domain: "com.forte.error", code: 999, userInfo: [NSLocalizedFailureReasonErrorKey: "未知错误"])
-//            delegate?.requestDidFailure?(self, error: error)
-//        }
-//    }
 }
 
 
